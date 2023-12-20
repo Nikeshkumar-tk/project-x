666032eec472010ad0cc1161a90571eb6ff5dd50
Vinayak S <vinayaksukhalal@gmail.com>
2023-12-20 22:15:41 +0530

    create project ui and backend created

--- src/app/pages/createProject/page.tsx
+++ src/app/pages/createProject/page.tsx
@@ -1,12 +1,144 @@
-import React from 'react'
+"use client"
 
-function createProject() {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import React,{useState} from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+ 
+import { cn } from "@/lib/utils"
+
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+export function createProject() {
+  const [date, setDate] = React.useState<Date>()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div className="flex items-center  h-screen">
-    acascascasc
-   
-  </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5 p-5 m-5">
+        <FormField
+          control={form.control}
+          
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel> Project name</FormLabel>
+              <FormControl>
+                <Input placeholder="projects name" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          <FormField
+          control={form.control}
+          
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel> Project Description</FormLabel>
+              <FormControl>
+                <Input placeholder="projects" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          <FormField
+          control={form.control}
+          
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel> Mentor name</FormLabel>
+              <FormControl>
+                <Input placeholder="projects" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div>
+        <label htmlFor="datePicker" className="block text-sm font-medium text-white-700">
+    Select a Date:
+  </label>
+        <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+        </div>
+      
+       
+    <br/>
+        <Button type="submit">Submit</Button>
+    
+           
+        
+      </form>
+    </Form>
   )
 }
-
 export default createProject
